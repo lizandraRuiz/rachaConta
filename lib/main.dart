d5554bea88ec8c504a356e2aec4719b2c49cc5d1
liruizz <lizandra.ruiz1@gmail.com>
2020-10-04 18:54:16 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // VARIAVEIS
+  final _totalConta = TextEditingController();
+  final _pessoas = TextEditingController();
+  final _garcom = TextEditingController();
+  var _infoText = "Informe seus valores!";
+  var _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Racha conta"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh),
+              onPressed: _resetFields)
+        ],
+      ),
+      body: _body(),
+    );
+  }
+
+  // PROCEDIMENTO PARA LIMPAR OS CAMPOS
+  void _resetFields(){
+    _totalConta.text = "";
+    _pessoas.text = "";
+    _garcom.text = "";
+    setState(() {
+      _infoText = "Informe seus valores!";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  _body() {
+    return SingleChildScrollView(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _editText("Valor total da conta", _totalConta),
+              _editText("Quantidade de pessoas", _pessoas),
+              _editText("Porcentagem do garçom", _garcom),
+              _buttonCalcular(),
+              _textInfo(),
+            ],
+          ),
+        ));
+  }
+
+  // Widget text
+  _editText(String field, TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      validator: (s) => _validate(s, field),
+      keyboardType: TextInputType.number,
+      style: TextStyle(
+        fontSize: 22,
+        color: Colors.blue,
+      ),
+      decoration: InputDecoration(
+        labelText: field,
+        labelStyle: TextStyle(
+          fontSize: 22,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  // PROCEDIMENTO PARA VALIDAR OS CAMPOS
+  String _validate(String text, String field) {
+    if (text.isEmpty) {
+      return "Preencha o campo '$field'";
+    }
+    return null;
+  }
+
+  // Widget button
+  _buttonCalcular() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0, bottom: 20),
+      height: 45,
+      child: RaisedButton(
+        color: Colors.blue,
+        child:
+        Text(
+          "Fechar a conta",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: () {
+          if(_formKey.currentState.validate()){
+            _calculate();
+          }
+        },
+      ),
+    );
+  }
+
+  // PROCEDIMENTO PARA CALCULAR O IMC
+  void _calculate(){
+    setState(() {
+      double totalConta = double.parse(_totalConta.text);
+      int pessoas = int.parse(_pessoas.text);
+      double garcom = double.parse(_garcom.text);
+
+      String totalGarcom = ((totalConta/100) * garcom).toStringAsFixed(2);
+      String totalPagar = (totalConta + ((totalConta/100) * garcom)).toStringAsFixed(2);
+      String totalTodos = ((totalConta + ((totalConta/100) * garcom))/pessoas).toStringAsFixed(2);
+
+      _infoText =
+        "Total a pagar: R\$$totalPagar\n"
+        "Total do garçom: R\$$totalGarcom\n"
+        "Ficou R\$$totalTodos por pessoa\n";
+    });
+  }
+
+  // // Widget text
+  _textInfo() {
+    return Text(
+      _infoText,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.blue, fontSize: 25.0),
+    );
+  }
+}
